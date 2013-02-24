b14ce9d59a40d7808ec0c0dc31db40291a6c71d4
Jaxkr <jacksonroberts25@gmail.com>
2013-02-23 22:23:49 -0700

    Changed public dir

--- app.js
+++ app.js
@@ -187,7 +187,7 @@ app.configure(function() {
   app.use(passport.initialize());
   app.use(passport.session());
   app.use(app.router);
-  app.use(express.static(__dirname + '/../../public'));
+  app.use(express.static(__dirname + '/public'));
 });
 
 
